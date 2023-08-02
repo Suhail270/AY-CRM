3dedc250c9c6a67ed0b7bd76429f4f751a9032c7
MHMujtaba <mujtaba.mohimtulay@gmail.com>
2023-08-02 18:44:29 +0530

    tasks list + tasks create

--- tasks/views.py
+++ tasks/views.py
@@ -14,43 +14,34 @@ from .forms import (
     TaskModelForm
 )
 
-class TaskCreateView(LoginRequiredMixin, generic.CreateView):
+class TaskCreateView(OrganizerAndLoginRequiredMixin, generic.CreateView):
     template_name = "tasks/task_create.html"
     form_class = TaskModelForm
-    
+
     def get_success_url(self):
         return reverse("tasks:task-list")
     
     def get_form(self, form_class=None):
         form = super().get_form(form_class)
         user = self.request.user
-        # form.fields['status'].queryset = Category.objects.filter(
-        #     organization=user.userprofile
-        # )
         # form.fields['agent'].queryset = Agent.objects.filter(
-        #     organization=user.userprofile
-        # )
-        # form.fields['party'].queryset = Parties.objects.filter(
         #     organization=user.userprofile
         # )
         return form
 
     def form_valid(self, form):
         task = form.save(commit=False)
-        if self.request.user.is_organizer:
-            task.organization = self.request.user.userprofile
-        else:
-            task.organization = Agent.objects.get(user = self.request.user).organization
         task.owner = self.request.user.userprofile
         task.save()
-        # send_mail(
-        #     subject="A lead has been created",
-        #     message="Go to the site to see the new lead",
-        #     from_email="test@test.com",
-        #     recipient_list=["test2@test.com"]
-        # )
+        send_mail(
+            subject="A task has been created",
+            message="Go to the site to see the new task",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
         messages.success(self.request, "You have successfully created a task")
         return super(TaskCreateView, self).form_valid(form)
+
 
 class TaskListView(LoginRequiredMixin, generic.ListView):
     template_name = "tasks/task_list.html"
@@ -59,37 +50,30 @@ class TaskListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         user = self.request.user
         # initial queryset of leads for the entire organization
-        # if user.is_organizer:
-        #     queryset = Task.objects.filter(
-        #         organization=user.userprofile
-        #     )
-        # else:
-        #     queryset = Task.objects.filter(
-        #         organization=user.agent.organization
-        #     )
-        #     # filter for the agent that is logged in
-        #     queryset = queryset.filter(agent__user=user)
-        # return queryset
-        return Task.objects
+        if user.is_organizer:
+            queryset = Task.objects.all()
+        else:
+            queryset = Task.objects.all()
+            # filter for the agent that is logged in
+            # queryset = queryset.filter(agent__user=user)
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super(TaskListView, self).get_context_data(**kwargs)
         user = self.request.user
-        # if user.is_organizer:
-        #     queryset = Task.objects.filter(
-        #         organization=user.userprofile, 
-        #         agent__isnull=True
-        #     )
-        #     context.update({
-        #         "unassigned_leads": queryset
-        #     })
+        if user.is_organizer:
+            queryset = Task.objects.all()
+            # context.update({
+            #     "unassigned_parties": queryset
+            # })
         return context
-    # views.py
+
 
 def task_list(request):
     tasks = Task.objects.all()
     todo_tasks = tasks.filter(status='TODO')
     in_progress_tasks = tasks.filter(status='IN_PROGRESS')
     done_tasks = tasks.filter(status='DONE')
+    print("here---------------------")
     return render(request, 'task_list.html', {'todo_tasks': todo_tasks, 'in_progress_tasks': in_progress_tasks, 'done_tasks': done_tasks})
 
