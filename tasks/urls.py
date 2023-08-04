c8e945ef565ccceddcf50dd7df61c5f5a061e897
lf823 <llf2000@hw.ac.uk>
2023-08-04 18:46:19 +0400

    sending emails and accepting task invites -s

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 from .views import (
     TaskCreateView,
     TaskListView
@@ -9,4 +10,5 @@ app_name = "tasks"
 urlpatterns = [
     path('', TaskListView.as_view(), name='task-list'),
     path('create/', TaskCreateView.as_view(), name='task-create'),
+    path('accept_invite/<int:task_id>/<str:token>/', TaskCreateView.accept_invite, name='accept-invite'),
 ]
